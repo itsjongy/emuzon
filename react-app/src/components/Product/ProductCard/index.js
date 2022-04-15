668fa6d9586faf1fd8d7a70f1f6fd04bad00bf8a
Jason Li <jasonjli218@gmail.com>
2022-04-15 14:23:25 -0700

    splashpage done

--- react-app/src/components/Product/ProductCard/index.js
+++ react-app/src/components/Product/ProductCard/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './style/ProductCard.css';
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ name, price, product_img, rating, id: item }) => {
     return (
-        <Link className='product-card-link' to={`/products/${product?.id}`}>
-            <div>
-                <img className='product-image' src={product?.product_img} alt='product' />
-                <p className='product-card-name'>{product?.name}</p>
-                <p className='product-card-price'>${product?.price}</p>
-                <div></div>
+        <Link className='product-card-link' to={`/products/${item}`}>
+            <div className='product-card-container'>
+                <div className='product-image-container'>
+                    <img className='product-card-image' src={product_img} alt='product' />
+                </div>
+                <div className='product-card-info'>
+                    <p className='product-card-name'>{name}</p>
+                    <p className='product-card-price'>${price}</p>
+                </div>
             </div>
         </Link>
     );
--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -10,6 +10,7 @@ const SplashPage = () => {
     const dispatch = useDispatch();
     const products = useSelector(state => Object.values(state.product));
     console.log('--------------', products)
+
     useEffect(() => {
         (async () => {
             await dispatch(getProducts());
@@ -21,9 +22,32 @@ const SplashPage = () => {
         return null;
     };
 
+    const rowOne = products.slice(0, 3);
+    const rowTwo = products.slice(3, 6);
+
     return (
-        <div>
-            <ProductCard />
+        <div className="splash-container">
+            <img className="splash-img" alt="splash-background" src="https://media.architecturaldigest.com/photos/5823ba99355a259d55d51886/16:9/w_3167,h_1781,c_limit/Blackband-Design-California-home-tour_03.jpg" />
+            <div className="splash-products">
+                {rowOne.map((each) => (
+                    <ProductCard
+                        key={each.id}
+                        name={each.name}
+                        price={each.price}
+                        product_img={each.product_img}
+                        id={each.id} />
+                ))}
+            </div>
+            <div className="splash-products">
+                {rowTwo.map((each) => (
+                    <ProductCard
+                        key={each.id}
+                        name={each.name}
+                        price={each.price}
+                        product_img={each.product_img}
+                        id={each.id} />
+                ))}
+            </div>
         </div>
     )
 }
