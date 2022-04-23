3d16c2d5378a52b7dde0cdca45f9d6ea1afc6298
Jason Li <jasonjli218@gmail.com>
2022-04-23 14:33:23 -0700

    validations done

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -70,7 +70,7 @@ export const logout = () => async (dispatch) => {
 };
 
 
-export const signUp = (first_name, last_name, email, password) => async (dispatch) => {
+export const signUp = (first_name, last_name, email, password, confirm_password) => async (dispatch) => {
   const response = await fetch('/api/auth/signup', {
     method: 'POST',
     headers: {
@@ -81,6 +81,7 @@ export const signUp = (first_name, last_name, email, password) => async (dispatc
       last_name,
       email,
       password,
+      confirm_password
     }),
   });
 
