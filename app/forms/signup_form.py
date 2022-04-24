a1b2f468370deefec8879b6ddd576b5edcbdb694
Jason Li <jasonjli218@gmail.com>
2022-04-23 21:32:23 -0700

    ??

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from wtforms.validators import DataRequired, ValidationError, EqualTo
 from app.models import User
 
 
@@ -21,6 +21,6 @@ def password_length(form, field):
 class SignUpForm(FlaskForm):
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists, Email()])
+    email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired(), password_length, EqualTo('confirm_password', message='Passwords do not match.')])
     confirm_password = StringField('confirm_password', validators=[DataRequired()])
