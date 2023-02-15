cd790d8b1bc52a7c0cfa19cad7c09950e853770d
Jason Li <jasonjli218@gmail.com>
2023-02-15 14:00:22 -0800

    model changes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .db import db, environment, SCHEMA, amazan_project
 from .user import User
 from .cart_item import Cart_item
 from .order import Order
